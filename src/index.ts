79de349d9004e2ff15f4014e1402680af11e11a0
Fagner Sales <fs.fagnersales@gmail.com>
2021-01-12 10:41:45 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import Discord from 'discord.js'
+
+const client = new Discord.Client()
+
+client.on('ready', () => {
+  console.log('I\'m ready!')
+})
+
+client.login('Nzk4NTQ1MjA0NDY5Njk0NTI0.X_2lLA.25SzKT__JYkDsZYfpVjzRfdbp5o')
